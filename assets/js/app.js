75f6f6c6e94f98f076d6ac5a12271a9837ccfdf2
unknown <cfortune072@gmail.com>
2022-11-21 15:28:24 +0100

    v2

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,9 +8,12 @@ let apiCall = function (city) {
     .then((data) => {
         // console.log(data);
         document.querySelector(`#city`).innerHTML = data.name;
-        document.querySelector(`#temp`).innerHTML = data.main.temp + `°`;
-        document.querySelector(`#humidity`).innerHTML =  data.main.humidity + `%`;
-        document.querySelector(`#wind`).innerHTML = data.wind.speed + `km/h`;
+        document.querySelector(`#temp`).innerHTML = 
+            "<i class='fas fa-thermometer-half'></i>" + data.main.temp + `°`;
+        document.querySelector(`#humidity`).innerHTML = 
+            "<i class='fas fa-tint'></i>" + data.main.humidity + `%`;
+        document.querySelector(`#wind`).innerHTML = 
+            "<i class='fas fa-wind'></i>" + data.wind.speed + `km/h`;
     })
 )
 
